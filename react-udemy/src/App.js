95f2e666700bb079e9e7d457a9b17755e6475c7a
Francisco Di Persia <fdipersia@gmail.com>
2018-12-27 15:54:15 -0300

    Add style and classes dynamically

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
   render() {
     const style = { 
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -66,12 +67,21 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
+    }
+    
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hello from React</h1>
-        <p>Whoa it is working!</p>
+        <p className={classes.join(' ')}>Whoa it is working!</p>
         <button
           style={style} 
           onClick={this.togglePersonsHandler}>Toggle Persons
